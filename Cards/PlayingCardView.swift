7e2ce091918fbb50d5c6d7d9405e133798916ec6
Akash Soni <akashsoni@novomac9s-macbook-pro-2.local>
2018-08-12 14:33:18 +0530

    bezeir pat clipping done      ......... emoji form :   emojipedia.org

--- Cards/PlayingCardView.swift
+++ Cards/PlayingCardView.swift
@@ -11,21 +11,10 @@ import UIKit
 class PlayingCardView: UIView {
 
     override func draw(_ rect: CGRect) {
-//        if let context = UIGraphicsGetCurrentContext(){
-//            context.addArc(center: CGPoint(x:bounds.midX, y: bounds.midY),
-//                           radius: 100.0, startAngle: 0, endAngle: 2*CGFloat.pi, clockwise: false)
-//            context.setLineWidth(5.0)
-//            UIColor.red.setStroke()
-//            UIColor.green.setFill()
-//            context.strokePath()
-//            context.fillPath()
-//        }
-        let path = UIBezierPath(arcCenter: CGPoint(x: bounds.midX, y: bounds.midY), radius: 100.0 , startAngle: 0, endAngle: 2 * CGFloat.pi, clockwise: false)
-        path.lineWidth = (10.0)
-        UIColor.red.setStroke()
-        UIColor.green.setFill()
-        path.stroke()
+        let path = UIBezierPath(roundedRect: bounds, cornerRadius: 15.0)
+        UIColor.white.setFill()
         path.fill()
+        path.addClip()
     }
 
 }
