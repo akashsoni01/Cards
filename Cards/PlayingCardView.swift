f9915c15d8e0a0dd631be4a3e770204c67ae6399
Akash Soni <akashsoni@novomac9s-macbook-pro-2.local>
2018-08-13 01:51:18 +0530

    constraints set for 5:8 ration >=H,V

--- Cards/PlayingCardView.swift
+++ Cards/PlayingCardView.swift
@@ -42,18 +42,23 @@ class PlayingCardView: UIView {
         label.sizeToFit()
         label.isHidden = !isFaceUp
     }
-    
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        setNeedsDisplay()
+        setNeedsLayout()
+    }
     override func layoutSubviews() {
         super.layoutSubviews()
         configureCornerLabel(upperLeftCornerLabel)
         upperLeftCornerLabel.frame.origin = bounds.origin.offsetBy(dx: cornerOffset, dy: cornerOffset)
         
         configureCornerLabel(lowerRightCornerLabel)
+        lowerRightCornerLabel.transform = CGAffineTransform.identity
+            .rotated(by: CGFloat.pi)
+        
         lowerRightCornerLabel.frame.origin = CGPoint(x: bounds.maxX, y: bounds.maxY)
         .offsetBy(dx: -cornerOffset, dy: -cornerOffset)
         .offsetBy(dx: -lowerRightCornerLabel.frame.size.width , dy: -lowerRightCornerLabel.frame.size.height)
-        lowerRightCornerLabel.transform = CGAffineTransform.identity.translatedBy(x: lowerRightCornerLabel.frame.size.width, y: lowerRightCornerLabel.frame.size.height)
-            .rotated(by: CGFloat.pi)
+
         
     }
     override func draw(_ rect: CGRect) {
